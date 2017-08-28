ac38464de1191f3bc150a31470c1d414c6daf0ab
kyle <kdeak1210@gmail.com>
2017-08-28 08:45:18 -0400

    App component is the main component
    It will hold other components...

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return(
-      <p>Hello</p>
+    return (
+      <form className="store-selector">
+        { /* This is only way to comment in JSX */ }
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store </button>
+      </form>
     );
   }
 }
