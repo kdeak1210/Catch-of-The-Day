3ca92dc973b297ea0f8aadab4e327528f4340999
kyle <kdeak1210@gmail.com>
2017-08-29 03:54:17 -0400

    Used Context to Surface BrowserRouter
    By telling React that StorePicker expects a router of type object, we can surface the context in StorePicker and access the router, and use the transitionTo method of react-router to route to the correct store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(e) {
+    e.preventDefault();
+    console.log('You changed the URL');
+    // First grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    // Second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         { /* This is only way to comment in JSX */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ (input) => {this.storeInput = input}} />
         <button type="submit">Visit Store </button>
       </form>
     );
   }
+}
+
+// 'Surface' our Context by telling React that StorePicker expects a router of type object
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
