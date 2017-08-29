5896ecfaef755ddd8a05e8748e44176d953d28f0
kyle <kdeak1210@gmail.com>
2017-08-29 05:03:47 -0400

    Handle state of fishes object in App
    The initial state is set in the constructor - fishes and order are initialized to empty objects. An AddFish method is added and binded to the App component. In this method, we update the state of fishes by making a copy of the existing fishes state, then adding the new fish to the copy and using setState to set to the modified copy. This is React best practices - it increases performance and protects against race conditions. Finally, we 'float' the new fish object from AddFishForm up to App through props.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,24 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this); // bind addFish method
+    // Initial State (aka getInitialState())
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+  addFish(fish) {
+    // update our state - make a copy of the existing state (spread operator)
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now(); // this will ALWAYS be unique
+    fishes[`fish-${timestamp}`] = fish;
+    // set state - explicitly tell React which state to update
+    this.setState({ fishes });  // syntactic sugar for fishes: fishes
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +30,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,8 +6,8 @@ class Inventory extends React.Component {
     return (
       <div>
         <h2>Inventory</h2>
-        <AddFishForm />
-      </div>  
+        <AddFishForm addFish={this.props.addFish}/>
+      </div>
     );
   }
 }
