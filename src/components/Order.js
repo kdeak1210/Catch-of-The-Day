a52f6260dda23d76049846dc39e0758e0f0a5006
kyle <kdeak1210@gmail.com>
2017-08-29 14:58:25 -0400

    Set up the Order Component
    Send fishes and order down from App into Order component as props. Return some JSX t
    o handle the order screen and proper format.. The renderOrder function was just creat
    ed to outsource some work from the render function of this component, and help keep t
    he code more organized. In order to use this in the context of this new function, we
    do need to add a constructor for the class (dont forget to call super!) and then bind
     'this' to the renderOrder function. The JSX declared in this component should render
     out the amount of each fish ordered, and the total price of the order.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { formatPrice } from '../helpers.js';
 
 class Order extends React.Component {
-  render() {
+  constructor() { // need the constructor just to bind this to renderOrder
+    super();
+    this.renderOrder = this.renderOrder.bind(this);
+  }
+
+  // Call this method inside render(), it just helps organize different logic
+  renderOrder(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+
+    if (!fish || fish.status === 'unavailable') {
+      return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available</li>
+    }
     return (
-      <p>Order</p>
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    );
+  }
+
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          // 'Outsource' some code to a new function for better organization
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     );
   }
 }
